48955ecd4153803722d3dc586d528d784e4f33a6
imjuoy <bsubhadeep95@gmail.com>
2018-03-13 07:36:17 +0000

    Changes in Python webservice and index html for jinja 2

--- Python.py
+++ Python.py
@@ -9,16 +9,10 @@ def home():
 
 @app.route('/signup', methods=['POST'])
 def signup():
-    session['username'] = request.form['username']
-    session['message'] = request.form['message']
-    return redirect(url_for('message'))
-
-@app.route('/message')
-def message():
-    if not 'username' in session:
-        return abort(403)
-    return render_template('message.html', username=session['username'], 
-                                           message=session['message'])
+    #session['username'] = request.form['username']
+    #session['message'] = request.form['message']
+    stream_of_words = request.form['words']
+    return render_template('result.html',words = stream_of_words)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', threaded=True, debug=True)
